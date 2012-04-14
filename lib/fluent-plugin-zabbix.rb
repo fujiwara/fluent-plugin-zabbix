41ad0095df39ace8c72007e7fc7cf4ab5b576d76
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2012-04-14 17:09:49 +0900

    remove files

--- lib/fluent-plugin-zabbix.rb
+++
@@ -1,9 +0,0 @@
-require "fluent-plugin-zabbix/version"
-
-module Fluent
-  module Plugin
-    module Zabbix
-      # Your code goes here...
-    end
-  end
-end
--- lib/fluent-plugin-zabbix/version.rb
+++
@@ -1,7 +0,0 @@
-module Fluent
-  module Plugin
-    module Zabbix
-      VERSION = "0.0.1"
-    end
-  end
-end
